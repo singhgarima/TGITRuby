6576d0d28bc84eeaf58368f846d182079fa3c01b
Garima Singh <garima.slideshare@gmail.com>
2011-08-08 14:56:48 +0530

    Garima - Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TGITRuby::Application.config.secret_token = '75e08a698448728b962d24c5fb5c7f51889938e227f45afa93726c32e1a974ce1212a703b9eda074931e2bcf45d4c91331f633afac04bf29360b93b53af7c8e4'
